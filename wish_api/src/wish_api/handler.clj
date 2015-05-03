b023280d7be7ed2d7a8dfcccde86cb08c1505980
Johan Driessen <johan@driessen.se>
2015-05-04 00:07:50 +0200

    Started with routes and skeleton methods for wishlist

--- wish_api/src/wish_api/handler.clj
+++ wish_api/src/wish_api/handler.clj
@@ -7,8 +7,24 @@
 		[ring.middleware.defaults :refer :all]
 		[ring.middleware.json :as middleware]))
 
+(defn get-all-wishes [userid]
+	(response []))
+
+(defn create-new-wish [userid wish]
+	(response wish))
+
+(defn get-wish [wishid]
+	(response {:title "Lorem ipsum" :description "Lorem ipsum dolor sit amet." :url "http://example.com" :userid "johndoe"}))
+
 (defroutes app-routes
 	(GET "/" [] (response {:hello "world"}))
+
+	(context "/:userid" [userid] (defroutes user-routes
+		(context "/wishes" [] (defroutes wishes-routes
+			(GET	"/" [] (get-all-wishes userid))
+			(POST	"/" {body :body} (create-new-wish userid body))
+			(context "/:wishid" [wishid] (defroutes wish-routes
+				(GET 	"/" [] (get-wish wishid))))))))
 
 	(route/not-found "Not Found"))
 
